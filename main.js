57f5fd8f6c1989a2c4767b5af08a16423dbe3391
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-05-05 04:37:11 +0000

    Exercise 3 Completed

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 // Write your code below
+console.log("Hello World!")
+
+// without the quotation marks, the Hello World! is now
+// being read as if it was part of the code, rather than 
+// text being used for the code.
